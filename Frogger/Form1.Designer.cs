1ee5a9c14e1d230ea69eb37774743ad256b82afe
James Hunt <jameshuntwork1997@yahoo.com>
2017-12-12 22:22:16 +0000

    Setting up - classes made (empty)
    Timer is live, empty classes for Frog and Log.

---
+++ Frogger/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Frogger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picGame = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picGame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picGame
+            // 
+            this.picGame.Location = new System.Drawing.Point(12, 12);
+            this.picGame.Name = "picGame";
+            this.picGame.Size = new System.Drawing.Size(1289, 772);
+            this.picGame.TabIndex = 0;
+            this.picGame.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1312, 796);
+            this.Controls.Add(this.picGame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picGame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picGame;
+    }
+}
+
---
+++ Frogger/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frogger
+{
+    public partial class Form1 : Form
+    {
+        // Classes / Objects Used:
+        #region initiate classes and objects
+
+        private Graphics Frogger;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        // Initiate Game:
+
+        #region initiate game and start timer
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer play = new Timer();
+            play.Interval = 10;
+            play.Tick += new EventHandler(refresh);
+            play.Start();
+        }
+
+        #endregion
+
+        #region timer_Tick method
+
+        private void refresh(object sender, EventArgs e)
+        {
+            this.draw();
+        }
+
+        #endregion
+
+        #region draw method (play game)
+
+        // Play Game:
+        private void draw()
+        {
+            Frogger = this.picGame.CreateGraphics();
+            Frogger.Clear(ColorTranslator.FromHtml("#333"));
+        }
+
+        #endregion
+    }
+}
